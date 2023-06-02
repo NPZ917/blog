67a71cce59ac97c906956bb74554bd2fabab4bdf
npz9977 <npz9977@163.com>
2023-06-03 00:53:45 +0800

    add fun.md

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -126,6 +126,7 @@ export default defineConfig({
       '/sem/': [{ text: '计算机网络', link: '/sem/network' }],
       '/other/': [
         { text: '文件上传', link: '/other/upload' },
+        { text: '函数式编程', link: 'other/fun.md' },
         { text: '性能优化', link: '/other/optimize' }
       ],
       '/bug/': [{ text: 'Node', link: '/bug/node/' }],
--- docs/daily_notes/index.md
+++ docs/daily_notes/index.md
@@ -1,1 +1,3 @@
 [进阶](https://juejin.cn/post/6844904103504527374)
+
+[vue-react](https://lq782655835.github.io/blogs/vue/diff-vue-vs-react.html)
